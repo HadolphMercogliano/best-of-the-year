ff4df0b2ca2d68b39033ee2e0d66beccd70f4974
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-21 16:42:11 +0200

    done step 1,2,3(no controllers)

--- src/main/java/com/learning/bestofhteyear/controller/MainController.java
+++ src/main/java/com/learning/bestofhteyear/controller/MainController.java
@@ -25,13 +25,13 @@ public class MainController {
   }
   @GetMapping("/movies")
   public String movies(Model model) {
-    model.addAttribute("movies", movieListToString(getBestMovies()));
+    model.addAttribute("movies", getBestMovies());
     return "movies";
   }
   
   @GetMapping("/songs")
   public String songs(Model model) {
-    model.addAttribute("songs", songListToString(getBestSongs()));
+    model.addAttribute("songs", getBestSongs());
     return "songs";
   }
   
